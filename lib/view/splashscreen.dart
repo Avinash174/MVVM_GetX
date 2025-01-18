817abba4771b800286c5604e5521d242de5b27e5
Avinash Magar <avinashmagar15@gmail.com>
2025-01-18 14:40:57 +0530

    new changes

--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_getx/data/app_exection.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +11,12 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          throw InternetException('No Internet Connection');
+        },
+      ),
+    );
   }
 }
