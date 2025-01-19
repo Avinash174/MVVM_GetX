7aa9798f3a61d743777645e44b0ff3f3ff364eab
Avinash Magar <avinashmagar15@gmail.com>
2025-01-19 20:07:04 +0530

    new changes

--- lib/res/color/app_color.dart
+++ lib/res/color/app_color.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 class AppColor {
+  static const Color whiteColor = Color(0xffffffff);
   static const Color blackColor = Color(0xff000000);
   static const Color tostMsgColor = Color(0xffffffff);
+  static const Color pinkColor = Color.fromARGB(255, 229, 14, 172);
 }
--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/utils.dart';
 import 'package:mvvm_getx/data/app_exection.dart';
+import 'package:mvvm_getx/res/color/app_color.dart';
 import 'package:mvvm_getx/utils/utils.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -15,14 +16,24 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('email_hint'.tr),
+        backgroundColor: AppColor.pinkColor,
+        title: Text(
+          'email_hint'.tr,
+          style: const TextStyle(
+            color: AppColor.whiteColor,
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColor.pinkColor,
         onPressed: () {
           Utils.toastCenterMessage(
             'message',
           );
         },
+        child: const Icon(
+          Icons.play_arrow_rounded,
+        ),
       ),
     );
   }
